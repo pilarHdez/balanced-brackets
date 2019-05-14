37548498e7371024164226094eec6f97560c06dc
Pilar Hernández Bastida <pilar_hdez_bastida@hotmail.com>
2019-05-13 19:33:59 -0500

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the isBalanced function below.
+    static String isBalanced(String s) {
+        int n = s.length();
+
+        if(n%2 != 0){
+            return "NO";
+        }
+        
+        Map<Character, Character> brackets = new HashMap<Character, Character>();
+        brackets.put('{','}');
+        brackets.put('[',']');
+        brackets.put('(',')');
+        
+        Stack<Character> stack = new Stack<Character>();
+        
+        for(int i = 0; i < n; i++){
+            
+           if(brackets.get(s.charAt(i)) != null){
+               stack.push(s.charAt(i));
+               
+           } else if(!brackets.get(stack.pop()).equals(new Character(s.charAt(i)))){
+               return "NO";
+           }
+        }
+        
+        return "YES";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String s = scanner.nextLine();
+
+            String result = isBalanced(s);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
